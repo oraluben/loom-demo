eeec57c8f7dea4ba87600f0b67dcdc87ce6d4937
Yichen Yan <yichen.yan@oracle.com>
2021-02-22 15:35:28 +0100

    Update

--- figs.py
+++ figs.py
@@ -1,10 +1,12 @@
+import os
 from pathlib import Path
 from subprocess import run, PIPE
 
 import seaborn as sns
 import pandas as pd
 
-JAVA_HOME = Path.home() / 'graal/loom/build/macosx-x86_64-server-release/images/graal-builder-jdk'
+JAVA_HOME = Path.home() / 'graal/loom/build/linux-x86_64-server-release/images/graal-builder-jdk'
+SVM_HOME = Path.home() / 'graal/graal-enterprise/substratevm-enterprise'
 
 
 def verify_get_loom(java='java'):
@@ -13,7 +15,13 @@ def verify_get_loom(java='java'):
     return JAVA_HOME / f'bin/{java}'
 
 
-def run_plot(start, end, step, title, out):
+def verify_get_mx():
+    assert SVM_HOME.exists()
+
+    return SVM_HOME.parents[1] / 'mx/mx'
+
+
+def run_loom_plot(start, end, step, title, out):
     r = run([verify_get_loom('java'), 'bench/loom/Main'] + [str(i) for i in (start, end, step)],
             cwd='src', stdout=PIPE)
     assert r.returncode == 0
@@ -30,5 +38,76 @@ def run_plot(start, end, step, title, out):
 
 
 if __name__ == '__main__':
-    assert run([verify_get_loom('javac'), 'bench/loom/Main.java'], cwd='src').returncode == 0
-    run_plot(100, 10000, 100, 'thread vs vthread on simple nop loop', 'full.png')
+    # assert run([verify_get_loom('javac'), 'bench/loom/Main.java'], cwd='src').returncode == 0
+    # run_loom_plot(100, 10000, 100, 'thread vs vthread on simple nop loop', 'loom.png')
+    #
+    # assert run([verify_get_loom('javac'), 'bench/Main.java'], cwd='src').returncode == 0
+    # assert run(['cp', '-r', 'bench', str(SVM_HOME)], cwd='src').returncode == 0
+    # assert run(
+    #     [verify_get_mx(), 'native-image',
+    #      '-H:Optimize=0', '-H:+UseLoom',
+    #      'bench.Main'], cwd=SVM_HOME,
+    #     env={'JAVA_HOME': str(JAVA_HOME.resolve()), 'PATH': os.getenv('PATH')}
+    # ).returncode == 0
+
+    # cont
+    results = []
+    for i in range(0, 200000, 10000):
+        r = run([verify_get_loom('java'), 'bench/Main', 'loom', 'cont', f'{i}'],
+                cwd='src', stdout=PIPE)
+        loom_results = r.stdout.decode().strip().split('\n')
+
+        r = run(['./bench.main', 'loom', 'cont', f'{i}'],
+                cwd=SVM_HOME, stdout=PIPE)
+        svm_results = r.stdout.decode().strip().split('\n')
+
+        results.append(['loom', i, int(loom_results[1])])
+        results.append(['svm', i, int(svm_results[1])])
+
+    fig = sns.lmplot(x="iteration", y="time", hue="type",
+                     data=pd.DataFrame(results, columns=['type', 'iteration', 'time']))
+    fig.set(title='nop continuation')
+    fig.savefig('cont.png')
+
+    # vthread
+    results = []
+    for i in range(0, 200000, 10000):
+        r = run([verify_get_loom('java'), 'bench/Main', 'loom', 'vthread', f'{i}'],
+                cwd='src', stdout=PIPE)
+        loom_results = r.stdout.decode().strip().split('\n')
+
+        r = run(['./bench.main', 'loom', 'vthread', f'{i}'],
+                cwd=SVM_HOME, stdout=PIPE)
+        svm_results = r.stdout.decode().strip().split('\n')
+
+        results.append(['loom', i, int(loom_results[1])])
+        results.append(['svm', i, int(svm_results[1])])
+
+    fig = sns.lmplot(x="iteration", y="time", hue="type",
+                     data=pd.DataFrame(results, columns=['type', 'iteration', 'time']))
+    fig.set(title='nop vthread')
+    fig.savefig('vthread.png')
+
+    # # yield depth
+    # results = []
+    # for i in range(1, 15):
+    #     r = run([verify_get_loom('java'), 'bench/Main', 'loom', 'yield-deep-cont', '10000', f'{i}'],
+    #             cwd='src', stdout=PIPE)
+    #     loom_results = r.stdout.decode().strip().split('\n')
+    #
+    #     r = run(['./bench.main', 'svm', 'yield-deep-cont', '10000', f'{i}'],
+    #             cwd=SVM_HOME, stdout=PIPE)
+    #     svm_results = r.stdout.decode().strip().split('\n')
+    #
+    #     results.append(['loom', i, int(loom_results[1])])
+    #     results.append(['svm', i, int(svm_results[1])])
+    #
+    # data = pd.DataFrame(results, columns=['type', 'depth', 'time'])
+    # loom_data = data[data['type'] == 'loom']
+    #
+    # fig = sns.lmplot(x="depth", y="time", hue="type", data=data)
+    # fig.set(title='yield at every level')
+    # fig.savefig('yield.png')
+    # fig = sns.lmplot(x="depth", y="time", hue="type", data=loom_data)
+    # fig.set(title='yield at every level')
+    # fig.savefig('yield-loom.png')
--- src/bench/Main.java
+++ src/bench/Main.java
@@ -1,30 +1,191 @@
 package bench;
 
+import java.util.Arrays;
+
 public class Main {
-    private static ContinuationScope FOO = new ContinuationScope("FOO");
+    private static final ContinuationScope SCOPE = new ContinuationScope("FOO");
+    private static boolean warmup;
 
-    public static void main(String[] args) {
-        System.out.println(run(1_000_000));
-    }
-
-    private static long run(int i) {
-        Continuation cont;
-        for (int j = 0; j < 2000; j++) {
-            cont = new Continuation(FOO, () -> {
-            });
-            while (!cont.isDone()) {
-                cont.run();
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println(Arrays.toString(args));
+        assert args.length > 2;
+        switch (args[0]) {
+            case "loom", "jdk" -> {
+                warmup = true;
+            }
+            case "svm", "native-image" -> {
+                warmup = false;
+            }
+            default -> {
+                throw new IllegalArgumentException(args[0]);
             }
         }
+        switch (args[1]) {
+            case "cont" -> {
+                if (warmup) runCont(20000);
+                System.out.println(runCont(Integer.parseInt(args[2])));
+            }
+            case "yield-cont" -> {
+                if (warmup) runYieldCont(20000);
+                System.out.println(runYieldCont(Integer.parseInt(args[2])));
+            }
+            case "vthread" -> {
+                if (warmup) runVThread(20000);
+                System.out.println(runVThread(Integer.parseInt(args[2])));
+            }
+            case "yield-vthread" -> {
+                if (warmup) runYieldVThread(20000);
+                System.out.println(runYieldVThread(Integer.parseInt(args[2])));
+            }
+            case "yield-deep-cont" -> {
+                if (warmup) runYieldDeepCont(20000, 10);
+                System.out.println(runYieldDeepCont(Integer.parseInt(args[2]), Integer.parseInt(args[3])));
+            }
+            case "yields-cont" -> {
+                if (warmup) runYieldsCont(20000, 10);
+                System.out.println(runYieldsCont(Integer.parseInt(args[2]), Integer.parseInt(args[3])));
+            }
+            default -> System.out.println("unsupported bench: " + args[0] + ".");
+        }
+    }
+
+    private static long runCont(int i) {
+        Continuation cont;
 
         long tStart = System.nanoTime();
         for (int j = 0; j < i; j++) {
-            cont = new Continuation(FOO, () -> {
+            cont = new Continuation(SCOPE, () -> {
             });
             while (!cont.isDone()) {
                 cont.run();
             }
         }
+        return System.nanoTime() - tStart;
+    }
+
+    private static long runYieldCont(int i) {
+        Continuation cont;
+
+        long tStart = System.nanoTime();
+        for (int j = 0; j < i; j++) {
+            cont = new Continuation(SCOPE, () -> {
+                Continuation.yield(SCOPE);
+            });
+            while (!cont.isDone()) {
+                cont.run();
+            }
+        }
+        return System.nanoTime() - tStart;
+    }
+
+    private static long runVThread(int i) throws InterruptedException {
+        long tStart = System.nanoTime();
+        for (int j = 0; j < i; j++) {
+            Thread.builder().virtual().task(() -> {
+            }).start().join();
+        }
+        return System.nanoTime() - tStart;
+    }
+
+    private static long runYieldVThread(int i) throws InterruptedException {
+        long tStart = System.nanoTime();
+        for (int j = 0; j < i; j++) {
+            Thread.builder().virtual().task(() -> {
+                Thread.yield();
+            }).start().join();
+        }
+        return System.nanoTime() - tStart;
+    }
+
+    private static long runYieldDeepCont(int i, int depth) {
+        Continuation cont;
+
+        long tStart = System.nanoTime();
+        for (int j = 0; j < i; j++) {
+            cont = Yielder.continuation(2, depth, false);
+            while (!cont.isDone())
+                cont.run();
+        }
+
         return (System.nanoTime() - tStart) / i;
+    }
+
+    private static long runYieldsCont(int i, int depth) {
+        Continuation cont;
+
+        long tStart = System.nanoTime();
+        for (int j = 0; j < i; j++) {
+            cont = Yielder.continuation(2, depth, true);
+            while (!cont.isDone())
+                cont.run();
+        }
+
+        return (System.nanoTime() - tStart) / i;
+    }
+
+    static class Arg {
+        volatile int field;
+    }
+
+    static class Yielder implements Runnable {
+        private final int paramCount;
+        private final int maxDepth;
+        private final boolean yieldAtEveryLevel;
+
+        private Yielder(int paramCount, int maxDepth, boolean yieldAtEveryLevel) {
+            if (paramCount < 1 || paramCount > 3)
+                throw new IllegalArgumentException();
+            this.paramCount = paramCount;
+            this.maxDepth = maxDepth;
+            this.yieldAtEveryLevel = yieldAtEveryLevel;
+        }
+
+        @Override
+        public void run() {
+            switch (paramCount) {
+                case 1 -> run1(maxDepth);
+                case 2 -> run2(maxDepth, new Arg());
+                case 3 -> run3(maxDepth, new Arg(), new Arg());
+                default -> throw new Error("should not happen");
+            }
+        }
+
+        private void run1(int depth) {
+            if (depth > 0) {
+                run1(depth - 1);
+            }
+            if (depth == 0) {
+                Continuation.yield(SCOPE);
+            } else if (yieldAtEveryLevel) {
+                Continuation.yield(SCOPE);
+            }
+        }
+
+        private void run2(int depth, Arg arg2) {
+            if (depth > 0) {
+                run2(depth - 1, arg2);
+            }
+            if (depth == 0) {
+                Continuation.yield(SCOPE);
+            } else if (yieldAtEveryLevel) {
+                Continuation.yield(SCOPE);
+            }
+        }
+
+        private void run3(int depth, Arg arg2, Arg arg3) {
+            if (depth > 0) {
+                run3(depth - 1, arg2, arg3);
+            }
+            if (depth == 0) {
+                Continuation.yield(SCOPE);
+            } else if (yieldAtEveryLevel) {
+                Continuation.yield(SCOPE);
+            }
+        }
+
+        static Continuation continuation(int paramCount, int maxDepth, boolean yieldAtEveryLevel) {
+            Runnable task = new Yielder(paramCount, maxDepth, yieldAtEveryLevel);
+            return new Continuation(SCOPE, 2000, task);
+        }
     }
 }
